8ceb269f51121d3bfc762ba5fa17e6e61cc31596
Karolis <Karolis@Stalinis>
2016-04-03 17:22:03 +0300

    image background, html,css changes

---
+++ Labanoro_Draugai/Labanoro_Draugai/src/main/java/lt/vu/mif/labanoro_draugai/reservation/HouseFilter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lt.vu.mif.labanoro_draugai.reservation;
+
+import javax.ejb.Stateful;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Karolis
+ */
+@Named
+@Stateful
+@SessionScoped
+public class HouseFilter {
+    
+}
