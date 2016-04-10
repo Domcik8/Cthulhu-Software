5d256fd189e5c43c84c827c7a6100d665b4516f7
Karolis <Karolis@Stalinis>
2016-04-10 15:20:27 +0300

    Filter dropdown, spinner, slider

--- Labanoro_Draugai/Labanoro_Draugai/src/main/java/lt/vu/mif/labanoro_draugai/reservation/HouseFilter.java
+++ Labanoro_Draugai/Labanoro_Draugai/src/main/java/lt/vu/mif/labanoro_draugai/reservation/HouseFilter.java
@@ -10,17 +10,18 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 
 /**
  *
  * @author Karolis
  */
 @ManagedBean(name = "houseFilter", eager = true)
-@SessionScoped
+@ViewScoped
 public class HouseFilter implements Serializable{
     private static final long serialVersionUID = 1L;
     
+    //Service checkbox list
     private List<String> availableFilters;
     public String[] selectedFilters;
 
@@ -33,6 +34,10 @@ public class HouseFilter implements Serializable{
         availableFilters.add("Service3");
         availableFilters.add("Service4");
         availableFilters.add("Service5");
+        System.out.println(toString() + " constructed.");
+        
+        setMaxHousePrice(1000);
+        setPriceTo(getMaxHousePrice());
     }
     
     public String[] getSelectedFilters() {
@@ -40,6 +45,11 @@ public class HouseFilter implements Serializable{
     }
 
     public void setSelectedFilters(String[] selectedFilters) {
+        if(selectedFilters!=null){
+            System.out.println("Selected services:");
+            for(String str:selectedFilters)
+                System.out.println("*"+str);
+        }
         this.selectedFilters = selectedFilters;
     }
     
@@ -47,7 +57,62 @@ public class HouseFilter implements Serializable{
         return availableFilters;
     }
 
+    //dropdown selection
     public void setAvailableFilters(List<String> availableFilters) {
         this.availableFilters = availableFilters;
     }
+    
+    private String ordering; 
+
+    public String getOrdering() {
+        return ordering;
+    }
+
+    public void setOrdering(String ordering) {
+        this.ordering = ordering;
+        System.out.println("Ordering by "+ ordering);
+    }
+    
+    //place count spinner
+    private int placeCount;
+
+    public int getPlaceCount() {
+        return placeCount;
+    }
+
+    public void setPlaceCount(int placeCount) {
+        System.out.println("Place count:  "+ placeCount);
+        this.placeCount = placeCount;
+    }
+    
+    //Price slider
+    private float maxHousePrice;
+
+    public float getMaxHousePrice() {
+        return maxHousePrice;
+    }
+
+    public void setMaxHousePrice(float maxHousePrice) {
+        this.maxHousePrice = maxHousePrice;
+    }
+    private float priceFrom = 0;
+
+    public float getPriceFrom() {
+        return priceFrom;
+    }
+
+    public void setPriceFrom(float priceFrom) {
+        this.priceFrom = priceFrom;
+    }
+
+    public float getPriceTo() {
+        return priceTo;
+    }
+
+    public void setPriceTo(float priceTo) {
+        this.priceTo = priceTo;
+    }
+    private float priceTo;
+    
+    
 }
