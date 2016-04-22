77f5b177f1a7f77debc727816ba1762fc932168a
Ne <necrqlt@gmail.com>
2016-04-22 22:25:10 +0300

    Login logic change

--- Labanoro_Draugai/Labanoro_Draugai/src/main/java/lt/vu/mif/labanoro_draugai/authentication/controller/LoginController.java
+++ Labanoro_Draugai/Labanoro_Draugai/src/main/java/lt/vu/mif/labanoro_draugai/authentication/controller/LoginController.java
@@ -9,34 +9,78 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import lt.vu.mif.labanoro_draugai.business.DatabaseManager;
+import lt.vu.mif.labanoro_draugai.entities.Person;
+
 /**
  *
  * @author NecrQ
  */
 @Stateless
 public class LoginController {
-    
-    @Inject 
+
+    @Inject
     private DatabaseManager db;
-    
-    public LoginController() {}
-    
-    public Boolean isUser(String email) {
-          
-        if (db.entityExists("Person", "Email", email)) {
-            return true; 
-        } 
+
+    public LoginController() {
+    }
+
+    public Boolean login(String email, String facebookId) {
+
+        if (isFbUser(email, facebookId)) {
+            return true;
+        } else {
+            registerUser(email, facebookId);
+
+            if (isFbUser(email, facebookId)) {
+                return true;
+            }
+        }
+
         return false;
     }
-    
-    public void RegisterUser(String email) {
-        
-        db.addPerson(email, null, null, "Person.Candidate");
+
+    public Boolean isFbUser(String email, String facebookId) {
+
+        if (isUser(email)) {
+
+            Person user = (Person) db.getEntity("Person", "Email", email);
+            if (user.getFacebookid().equals(facebookId)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
     }
-    
-    public void RegisterUser(String email, String firstName, String lastName) {
-        
+
+    public Boolean isUser(String email) {
+
+        if (db.entityExists("Person", "Email", email)) {
+            return true;
+        }
+        return false;
+    }
+
+    // Simple user registration [check by email]
+    public void registerUser(String email) {
+
+        if (!isUser(email)) {
+            db.addPerson(email, null, null, "Person.Candidate");
+        }
+    }
+
+    // Facebook user registration [check by email and facebookId] 
+    public void registerUser(String email, String facebookId) {
+
+        if (!isUser(email)) {
+            Person person = db.addPerson(email, null, null, "Person.Candidate");
+            person.setFacebookid(facebookId);
+        }
+    }
+
+    public void registerUser(String email, String firstName, String lastName) {
+
         db.addPerson(email, firstName, lastName, "Person.Candidate");
     }
-    
+
 }
