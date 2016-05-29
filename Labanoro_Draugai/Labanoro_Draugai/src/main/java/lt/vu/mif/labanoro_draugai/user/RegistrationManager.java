cc964aac48892a5de67ced1d06e4f68c853aa707
Dominik Lisowski <dominik.lisovski@gmail.com>
2016-05-29 22:40:56 +0300

    Merge origin/master
    Conflicts:
    	Labanoro_Draugai/Labanoro_Draugai/src/main/java/lt/vu/mif/labanoro_draugai/user/RegistrationManager.java

--- Labanoro_Draugai/Labanoro_Draugai/src/main/java/lt/vu/mif/labanoro_draugai/user/RegistrationManager.java
+++ Labanoro_Draugai/Labanoro_Draugai/src/main/java/lt/vu/mif/labanoro_draugai/user/RegistrationManager.java
@@ -155,10 +155,7 @@ public class RegistrationManager implements Serializable {
             return null;
         }
 
-        //person = (Person) dbm.updateEntity(person);
         emailBean.sendRegisterConfirmationMessage(person);
-        
-        emailBean.sendEmailConfirmationMessage(person);
         person = (Person) dbm.updateEntity(person); //Pakeitimai del optimistic locking
         
         return (dbm.getSystemParameter("SystemParameter.Redirect.Login").getValue() + "?faces-redirect=true");
